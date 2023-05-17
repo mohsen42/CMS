80ca1849ba9986ed866a2889f7b3f4598e44391f
mohsen dolatshah <mohsen2latshah@gmail.com>
2023-05-17 16:34:57 +0430

    add swagger to method

--- src/main/java/com/example/cms/Resources/CategoryResource.java
+++ src/main/java/com/example/cms/Resources/CategoryResource.java
@@ -1,7 +1,10 @@
 package com.example.cms.Resources;
 
 import com.example.cms.models.Category;
+import com.example.cms.service.CategoryService;
 import com.example.cms.vo.CategoryRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,28 +16,74 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
+@Api(tags = "category", description = "Category API")
 public class CategoryResource {
 
+    private final CategoryService categoryService;
+    public CategoryResource(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping(value = "/{id}")
+    @ApiOperation(value = "Find category", notes = "Find the Category by ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Category found"),
+            @ApiResponse(code = 404, message = "Category not found"),
+    })
     public ResponseEntity<Category> findOne(@PathVariable String id) {
         return ResponseEntity.ok(new Category());
     }
 
+
+    @GetMapping
+    @ApiOperation(value = "List categories", notes = "List all categories")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Categories found"),
+            @ApiResponse(code = 404, message = "Category not found")
+    })
+    public ResponseEntity<List<Category>> findAll() {
+        return ResponseEntity.ok(this.categoryService.findAll());
+    }
+
     @PostMapping
+    @ApiOperation(value = "Create category",notes = "It permits to create a new category")
+            @ApiResponses(value = {
+                    @ApiResponse(code = 201,message = "Category created successfully"),
+                            @ApiResponse(code = 400,message = "Invalid request")
+            })
     public ResponseEntity<Category> newCategory(CategoryRequest
-                                                        category){
+                                                        category) {
         return new ResponseEntity<>(new Category(), HttpStatus.CREATED);
     }
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void removeCategory(@PathVariable("id") String id){
+    @ApiOperation(value = "Remove category",notes = "It permits to remove a category")
+            @ApiResponses(value = {
+                    @ApiResponse(code = 200,message = "Category removed successfully"),
+                            @ApiResponse(code = 404,message = "Category not found")
+            })
+    public void removeCategory(@PathVariable("id") String id) {
     }
+
     @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation(value = "Update category",notes = "It permits to update a category")
+            @ApiResponses(value = {
+                    @ApiResponse(code = 200,message = "Category update successfully"),
+                    @ApiResponse(code = 404,message = "Category not found"),
+                    @ApiResponse(code = 400,message = "Invalid request")
+            })
     public ResponseEntity<Category> updateCategory(@PathVariable("id")
-                                                   String id, CategoryRequest category){
+                                                   String id, CategoryRequest category) {
         return new ResponseEntity<>(new Category(), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/cms/swagger/SwaggerConfiguration.java
+++ src/main/java/com/example/cms/swagger/SwaggerConfiguration.java
@@ -11,7 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 public class SwaggerConfiguration {
 
-
     @Bean
     public Docket documentation() {
         return new Docket(DocumentationType.SWAGGER_2)
